39b337d95628b87dfe97d624363a4c02fd11d6c3
1w1111957TodescoNatalia <nstodesco@gmail.com>
2022-04-07 17:39:34 -0300

    Modificar Proyecto

--- src/components/listas/listas.jsx
+++ src/components/listas/listas.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import Footer from '../footer/footer';
 import Navbar from '../Navbar/navbar';
+import { Link } from 'react-router-dom'
+
 import "./listas.css"
 
 function Listas() {
@@ -12,6 +14,12 @@ function Listas() {
                     <div className="col-lg-12 text-center">
                         <h1 style={{fontWeight: '800'}}>MIS LISTAS</h1>
                     </div>
+                </div>
+                <div className="row">
+                    <Link to={"/project/edit/PGBE7PEBI1RWUQaHC6jQ"}>
+                    {/* <Link to={"/usuario/" + user.id}> */}
+                    <button className="btn btn-dark">Editar</button>
+                    </Link>
                 </div>
             </div>
             <Footer></Footer>
