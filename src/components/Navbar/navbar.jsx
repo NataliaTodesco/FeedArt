ffbf58ec5ae4b777ee2bbcd22149c74b6f5274db
1w1111957TodescoNatalia <nstodesco@gmail.com>
2022-04-28 13:01:20 -0300

    Gestion de Listas

--- src/components/Navbar/navbar.jsx
+++ src/components/Navbar/navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import { NavLink, useNavigate } from "react-router-dom";
 import logo from "../../img/logoHorizontal.svg";
 import "./navbar.css";
@@ -13,167 +13,189 @@ import {
   MenuItem,
   Avatar,
 } from "@mui/material";
-import MenuIcon from '@mui/icons-material/Menu';
-import { logOut } from '../../firebaseConfig';
+import MenuIcon from "@mui/icons-material/Menu";
+import { useUsuario } from '../../context/UserContext'
 
 function Navbar() {
   const navigate = useNavigate();
-  
-  function log_Out(){
+  const { logOut, usuario } = useUsuario();
+
+  function log_Out() {
     logOut();
-    navigate('/');
-    console.log("bandera")
+    navigate("/");
   }
-    const [anchorElNav, setAnchorElNav] = React.useState(null);
-    // const [anchorElUser, setAnchorElUser] = React.useState(null);
-  
-    const handleOpenNavMenu = (event) => {
-      setAnchorElNav(event.currentTarget);
-    };
-    // const handleOpenUserMenu = (event) => {
-    //   setAnchorElUser(event.currentTarget);
-    // };
-  
-    const handleCloseNavMenu = () => {
-      setAnchorElNav(null);
-    };
-  
-    // const handleCloseUserMenu = () => {
-    //   setAnchorElUser(null);
-    // };
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
 
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
 
-    let storage = localStorage.getItem('storage')
-    storage = JSON.parse(storage)
-  
-  return (   
-    <div className='container-fluid'>
-      <AppBar position="static" style={{backgroundColor: "transparent", boxShadow: 'none', width: '99%'}}>
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  return (
+    <div className="container-fluid menu">
+      <AppBar
+        position="static"
+        style={{
+          backgroundColor: "transparent",
+          boxShadow: "none",
+        }}
+      >
         <Toolbar disableGutters>
           <Typography
             variant="h6"
             noWrap
             component="div"
-            sx={{ mr: 2, display: { xs: 'none', md: 'flex' } }}
+            sx={{ mr: 2, display: { xs: "none", md: "flex" } }}
           >
             <img src={logo} className="img-fluid" alt="" />
           </Typography>
-        <Container maxWidth="xl">
-          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              color="inherit"
-            >
-              <MenuIcon />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'left',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'left',
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={{
-                display: { xs: 'block', md: 'none' },
-              }}
-            >
-              <MenuItem onClick={handleCloseNavMenu}>
-                <NavLink className="link" activeClassName="active" to="/home">
-                <i className="bi bi-house-fill"></i> Inicio
-                </NavLink>
-              </MenuItem>
-              <MenuItem onClick={handleCloseNavMenu}>
-                <NavLink className="link" activeClassName="active" to="/list">
-                <i className="bi bi-list-stars"></i> 
-                  Mis Listas
-                </NavLink>
-                </MenuItem>
-                {storage.uid === "qA2c3TwTAKUc9160fsJlMtDSVgl1"? <NavLink className="link" activeClassName="active" to="/admin">
-                <MenuItem onClick={handleCloseNavMenu}>
-                <i className="bi bi-clipboard-data-fill"></i>
-                  Gestión
-                </MenuItem>
-                </NavLink> : <span></span>}
-                <MenuItem onClick={handleCloseNavMenu}>
-                <NavLink className="link" activeClassName="active" to="/new-project">
-                  <i className="bi bi-plus-circle-fill"></i> 
-                  Nuevo Proyecto
-                </NavLink>
-                </MenuItem>
-                <NavLink className="link d-flex justify-content-center"  to="/user">
-                  <Avatar alt="Profile Picture" src={storage.photoURL} />
-                </NavLink>
-                <button onClick={log_Out} className="btn btn-danger btn-block px-4 mt-3">
-                  Salir
-                  <i className="bi bi-arrow-bar-right ml-1" style={{fontSize:'medium'}}></i>
-                </button>
-            </Menu>
-          </Box>
-          <Typography
-            variant="h6"
-            noWrap
-            component="div"
-            sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}
-          >
-            <img src={logo} className="img-fluid" alt="" />
-          </Typography>
-          <Box className='mt-3 float-right' sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-            {/* {pages.map((page) => (
-              <Button
-                key={page}
-                onClick={handleCloseNavMenu}
-                sx={{ my: 2, color: 'white', display: 'block' }}
+          <Container maxWidth="xl">
+            <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+              <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleOpenNavMenu}
+                color="inherit"
               >
-                {page}
-              </Button>
-            ))} */}
+                <MenuIcon />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorElNav}
+                anchorOrigin={{
+                  vertical: "bottom",
+                  horizontal: "left",
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "left",
+                }}
+                open={Boolean(anchorElNav)}
+                onClose={handleCloseNavMenu}
+                sx={{
+                  display: { xs: "block", md: "none" }
+                }}
+              >
+                <MenuItem onClick={handleCloseNavMenu}>
+                  <NavLink className="link" activeClassName="active" to="/home">
+                    <i className="bi bi-house-fill"></i> Inicio
+                  </NavLink>
+                </MenuItem>
+                <MenuItem onClick={handleCloseNavMenu}>
+                  <NavLink className="link" activeClassName="active" to="/list">
+                    <i className="bi bi-list-stars"></i>
+                    Mis Listas
+                  </NavLink>
+                </MenuItem>
+                {usuario.uid === "qA2c3TwTAKUc9160fsJlMtDSVgl1" ? (
+                  <NavLink
+                    className="link"
+                    activeClassName="active"
+                    to="/admin"
+                  >
+                    <MenuItem onClick={handleCloseNavMenu}>
+                      <i className="bi bi-clipboard-data-fill"></i>
+                      Gestión
+                    </MenuItem>
+                  </NavLink>
+                ) : (
+                  <span></span>
+                )}
+                <MenuItem onClick={handleCloseNavMenu}>
+                  <NavLink
+                    className="link"
+                    activeClassName="active"
+                    to="/new-project"
+                  >
+                    <i className="bi bi-plus-circle-fill"></i>
+                    Nuevo Proyecto
+                  </NavLink>
+                </MenuItem>
+                <NavLink
+                  className="link d-flex justify-content-center"
+                  to="/user"
+                >
+                  <Avatar alt="Profile Picture" src={usuario.photoURL} />
+                </NavLink>
+                <button
+                  onClick={log_Out}
+                  className="btn btn-danger btn-block px-4 mt-3"
+                >
+                  Salir
+                  <i
+                    className="bi bi-arrow-bar-right ml-1"
+                    style={{ fontSize: "medium" }}
+                  ></i>
+                </button>
+              </Menu>
+            </Box>
+            <Typography
+              variant="h6"
+              noWrap
+              component="div"
+              sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}
+            >
+              <img src={logo} className="img-fluid" alt="" />
+            </Typography>
+            <Box
+              className="mt-3 float-right"
+              sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}
+            >
               <MenuItem onClick={handleCloseNavMenu}>
                 <NavLink className="link" activeClassName="active" to="/Home">
-                <i className="bi bi-house-fill"></i>
+                  <i className="bi bi-house-fill"></i>
                   Inicio
                 </NavLink>
               </MenuItem>
               <MenuItem onClick={handleCloseNavMenu}>
                 <NavLink className="link" activeClassName="active" to="/list">
-                <i className="bi bi-list-stars"></i> 
-                Mis Listas
+                  <i className="bi bi-list-stars"></i>
+                  Mis Listas
                 </NavLink>
-                </MenuItem>
-                {storage.uid === "qA2c3TwTAKUc9160fsJlMtDSVgl1"? <NavLink className="link" activeClassName="active" to="/admin">
-                <MenuItem onClick={handleCloseNavMenu}>
-                <i className="bi bi-clipboard-data-fill"></i>
-                Gestión
-                </MenuItem>
-                </NavLink> : <span></span>}
-                <MenuItem onClick={handleCloseNavMenu}>
-                <NavLink className="link" activeClassName="active" to="/new-project">
+              </MenuItem>
+              {usuario.uid === "qA2c3TwTAKUc9160fsJlMtDSVgl1" ? (
+                <NavLink className="link" activeClassName="active" to="/admin">
+                  <MenuItem onClick={handleCloseNavMenu}>
+                    <i className="bi bi-clipboard-data-fill"></i>
+                    Gestión
+                  </MenuItem>
+                </NavLink>
+              ) : (
+                <span></span>
+              )}
+              <MenuItem onClick={handleCloseNavMenu}>
+                <NavLink
+                  className="link"
+                  activeClassName="active"
+                  to="/new-project"
+                >
                   <i className="bi bi-plus-circle-fill"></i>
                   Nuevo Proyecto
                 </NavLink>
-                </MenuItem>
-                <NavLink className="link"  to="/user">
-                  <Avatar alt="Profile Picture" src={storage.photoURL} />
-                </NavLink>
-                <button onClick={log_Out} className="btn btn-danger pl-4 pr-3 ml-4">
-                  Salir 
-                  <i className="bi bi-arrow-bar-right ml-1" style={{fontSize:'medium'}}></i>
-                </button>
-          </Box>
+              </MenuItem>
+              <NavLink className="link" to="/user">
+                <Avatar alt="Profile Picture" src={usuario.photoURL} />
+              </NavLink>
+              <button
+                onClick={log_Out}
+                className="btn btn-danger pl-4 pr-3 ml-4"
+              >
+                Salir
+                <i
+                  className="bi bi-arrow-bar-right ml-1"
+                  style={{ fontSize: "medium" }}
+                ></i>
+              </button>
+            </Box>
           </Container>
         </Toolbar>
-      
-    </AppBar>
+      </AppBar>
     </div>
   );
 }
--- src/components/listas/listas.jsx
+++ src/components/listas/listas.jsx
@@ -1,42 +1,49 @@
 import React from 'react'
 import Footer from '../footer/footer';
 import Navbar from '../Navbar/navbar';
-import { Link } from 'react-router-dom'
-import {  Button, Modal } from 'antd';
-import { DeleteOutlined, EditOutlined, ExclamationCircleOutlined } from '@ant-design/icons';
-
 import "./listas.css"
-import { borrarProyecto } from '../../firebaseConfig';
+import Favoritos from './favoritos';
+import Likes from './likes';
+import MisProyectos from './misProyectos';
 
 function Listas() {
-    function eliminar(id) {
-        Modal.confirm({
-            title: 'Eliminar',
-            icon: <ExclamationCircleOutlined />,
-            content: '¿Estas seguro de que deseas eliminar este Proyecto?',
-            okText: 'Eliminar',
-            cancelText: 'Cancelar',
-            onOk() {
-                borrarProyecto(id)
-            }
-        });
-      }
-
     return (  
         <div className='listas'>
             <Navbar></Navbar>
-            <div className="container my-3">
+            <div className="container my-3" style={{ minHeight: "62.5vh" }}>
             <div className="row">
                     <div className="col-lg-12 text-center">
                         <h1 style={{fontWeight: '800'}}>MIS LISTAS</h1>
                     </div>
                 </div>
                 <div className="row">
-                    <Link to={"/project/edit/PGBE7PEBI1RWUQaHC6jQ"}>
-                    {/* <Link to={"/usuario/" + user.id}> */}
-                        <Button type="primary" shape="round" icon={<EditOutlined />} size={"large"} />                        
-                    </Link>
-                    <Button onClick={e => {eliminar("O9feBPKesCUzdTEEE7mw")}} type="primary" shape="round" icon={<DeleteOutlined />} danger size={"large"} />
+                    <div className="col-lg-4">
+                        <h3>
+                            <i className="bi bi-hand-thumbs-up-fill mr-3"></i>
+                            ME GUSTA
+                        </h3>
+                        <div className="list">
+                            <Likes></Likes>
+                        </div>
+                    </div>
+                    <div className="col-lg-4">
+                        <h3>
+                            <i className="bi bi-heart-fill mr-3"></i>
+                            MIS FAVORITOS
+                        </h3> 
+                        <div className="list">
+                            <Favoritos></Favoritos>
+                        </div>
+                    </div>
+                    <div className="col-lg-4">
+                        <h3>
+                            <i className="bi bi-images mr-3"></i>
+                            MIS PROYECTOS
+                        </h3>
+                        <div className="list">
+                            <MisProyectos></MisProyectos>
+                        </div>
+                    </div>
                 </div>
             </div>
             <Footer></Footer>
